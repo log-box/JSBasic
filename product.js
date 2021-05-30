af7a5dd361d1a25c58b5902d1d4073c97606b5bc
artem <a.y.logvin@gmail.com>
2021-05-30 19:55:32 +0300

    done

--- product.js
+++ product.js
@@ -1,11 +1,27 @@
+//массив продукции
 var products = [
-    {prod_name:'22" (55 см) Телевизор LED DEXP F22F7000K черный', prod_price: 100000},
-    'Ключ активации DOOM (1993) (Xbox ONE)',
-    'Кабель Samsung USB Type-C - USB Type C черный 1 м',
-    '27" Монитор LG 27QN600-B [27QN600-B.ARUZ]',
-    '2 ТБ Внешний HDD WD Elements Portable [WDBU6Y0020BBK]',
+    {
+        prod_name: '22" (55 см) Телевизор LED DEXP F22F7000K черный',
+        prod_price: 100000
+    },
+    {
+        prod_name: 'Ключ активации DOOM (1993) (Xbox ONE)',
+        prod_price: 10000
+    },
+    {
+        prod_name: 'Кабель Samsung USB Type-C - USB Type C черный 1 м',
+        prod_price: 1000
+    },
+    {
+        prod_name: '27" Монитор LG 27QN600-B [27QN600-B.ARUZ]',
+        prod_price: 100
+    },
+    {
+        prod_name: '2 ТБ Внешний HDD WD Elements Portable [WDBU6Y0020BBK]',
+        prod_price: 10
+    },
 ]
-
+//отрисовка раздела продукции
 function prod_render() {
     var prodDiv = document.getElementById('prod');
     prodDiv.style.borderColor = 'black';
@@ -16,7 +32,7 @@ function prod_render() {
     prodDiv.style.minWidth = '100px';
     prodDiv.style.marginTop = '50px';
     prodDiv.style.position = 'absolute'
-    prodDiv.style.left = '1000px'
+    prodDiv.style.left = '1100px'
     prodDiv.style.color = 'cyan';
     prodDiv.style.justifyContent = 'center'
     prodDiv.style.display = 'flex';
@@ -26,35 +42,37 @@ function prod_render() {
     newEl.textContent = 'Продукция: ';
     prodDiv.appendChild(newEl);
     prodDiv.style.justifyContent = 'center';
-    function render() {
-        var card = document.createElement("div");
-        card.className = 'card';
-        prod.append(card);
-        card.style.width = '44px';
-        card.style.height = '44px';
-        card.style.borderColor = 'black';
-        card.style.borderStyle = 'solid';
-        card.style.margin = '0px';
-        card.style.padding = '0px';
-        card.style.display = 'flex';
-        card.style.justifyContent = 'center';
-        card.style.alignItems = 'center';
-        card.style.textEmphasisColor = 'white';
-        card.style.color = 'white';
-        // card.insertAdjacentHTML
-        // Product.forEach(function(item) {
-        //     prodDiv.insertAdjacentHTML('beforeend', item + ' <br> ');
-        // });
-    }
-    window.onload = render();
-}
-window.onload = prod_render();
 
-function Product(name, price, array=products) {
+}
+
+window.onload = prod_render();
+//отрисовка карточек
+function render(num) {
+    var card = document.createElement("div");
+    card.className = 'card';
+    card.id = 'card' + num
+    prod.append(card);
+    card.style.borderColor = 'black';
+    card.style.borderStyle = 'solid';
+    card.style.margin = '0px';
+    card.style.padding = '0px';
+    card.style.display = 'flex';
+    card.style.justifyContent = 'center';
+    card.style.alignItems = 'center';
+}
+//заполнение карточек данными из массива
+for (let n = 0; n < products.length; n++) {
+    window.onload = render(n);
+    let target = document.getElementById('card' + n);
+    target.insertAdjacentHTML('afterbegin', products[n].prod_name + '<br>' + products[n].prod_price)
+
+};
+//Функция создания продукции с методом добаления в массив
+function Product(name, price, array = products) {
     this.name = name;
     this.price = price;
     this._array = array;
-    
+
     this.pushToArray = function () {
         return this._array.push({
             prod_name: this.name,
@@ -63,5 +81,6 @@ function Product(name, price, array=products) {
     }
 }
 
-let a = Product('test1', 121);
+let a = new Product('test1', 121);
 a.pushToArray();
+
